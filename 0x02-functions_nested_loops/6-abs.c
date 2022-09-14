4df15cbf1e9d4391dbdbbf1dce7c48cc9bc6ecf2
maryjanechukelu <maryjanechukelu@gmail.com>
2022-09-14 14:12:18 -0700

    6 resaved

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,18 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
-* print_last_digit - Prints the last digit of a number.
-* @n: The number in question.
-* Return: Value of the last digit.
-*/
-
-int print_last_digit(int n)
+ * _abs - function that computes the actual value of an integer
+ * @c: is the int that will use for the argument of the function
+ * Return: 0
+ */
+int _abs(int c)
 {
-int last_digit = n % 10;
-if (last_digit < 0)
-last_digit *= -1;
-_putchar(last_digit + '0');
-return (last_digit);
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+		return (c * -1);
 }
 
 
